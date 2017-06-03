cacaf0a50b52abed924b1f785cff422db73de6c8
Jason Shepherd <jshep1202@icloud.com>
2017-06-03 06:57:22 -0400

    Added Pokecell view, connected outlets to cell

---
+++ Pokecell.swift
@@ -0,0 +1,13 @@
+//
+//  Pokecell.swift
+//  Pokedex
+//
+//  Created by Jason Shepherd on 6/3/17.
+//  Copyright © 2017 Jason Shepherd. All rights reserved.
+//
+
+import UIKit
+
+class Pokecell: UICollectionViewCell {
+    
+}
