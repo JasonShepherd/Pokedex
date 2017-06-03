9d3066f0d4879870d3e1a177372e72b88b059378
Jason Shepherd <jshep1202@icloud.com>
2017-06-03 08:01:24 -0400

    added rounded corners to Pokecells.

--- Pokecell.swift
+++ Pokecell.swift
@@ -9,5 +9,29 @@
 import UIKit
 
 class Pokecell: UICollectionViewCell {
+ 
+    @IBOutlet weak var thumbnailImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    var pokemon : Pokemon!
+    
+    //rounded corners
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbnailImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+        
+    }
+    
+    
+    
     
 }
